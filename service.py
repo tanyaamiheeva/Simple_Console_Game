255c4ac108c368d8de44f1a458fb57d967fbbc1b
Mikheeva Tatyana <Mikheeva.TA@phystech.edu>
2020-05-02 14:51:11 +0300

    second task added

---
+++ service.py
@@ -0,0 +1,10 @@
+from enum import Enum
+
+
+class ExistingWeapons:
+    weapons = ["sword", "special power", "fangs", "crossbow", "cudgel", "fire"]
+
+
+class Side(Enum):
+    Demigods = 1
+    Monsters = 0
